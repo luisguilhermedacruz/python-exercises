33a7348372f218d8eddea4284fa391596f65d255
Luis Guilherme da Cruz <guicruz95@hotmail.com>
2026-03-12 20:11:49 -0300

    feat: I created a functiont that reverse the word

---
+++ exercises7.py
@@ -0,0 +1,11 @@
+word = input("")
+
+def reversed_word(word: str) -> str:
+    new_word = ""
+
+    for n in word:
+        new_word = n + new_word
+
+    return new_word
+
+print(reversed_word(word))

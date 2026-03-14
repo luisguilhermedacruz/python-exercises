36e196a025d2bbe7669728995a187317e606daf5
Luis Guilherme da Cruz <guicruz95@hotmail.com>
2026-03-14 17:18:51 -0300

    add: I tested other dics examples

--- exercise9.py
+++ exercise9.py
@@ -7,4 +7,13 @@ person = {
 def get_age(person: dict) -> int:
     return person["age"]
 
+
+cars_honda = {
+    "modelo":"civic",
+    "age": "2026",
+    "color": "white"
+
+}
+
 print(get_age(person))
+print(f"My dream's car is {cars_honda["modelo"]} of age {cars_honda["age"]} and color {cars_honda["color"]}")

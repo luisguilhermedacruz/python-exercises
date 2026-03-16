fdcfa7186acfa438e7f85fe20896206d214c0e0f
Luis Guilherme da Cruz <guicruz95@hotmail.com>
2026-03-16 13:04:13 -0300

    fix: I was need to correct the file

--- exercise10.py
+++ exercise10.py
@@ -1,4 +1,2 @@
-numbers = [0,10,66,3,667,1223,5654,12,56]
-numbersEven = [p * 2 for p in numbers if p % 2 == 0]
-
-print(numbersEven)
+numbers = [0,10,24,505,395,1230,4554]
+numbersEven = [e for e in numbers if e % 2 == 0]

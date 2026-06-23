6a367384fdbc5359a1684b9c48c9894b0ba37f47
Luis Guilherme da Cruz <guicruz95@hotmail.com>
2026-06-23 13:58:08 -0300

    I created small review tests for me remember

--- NewTests/test.py
+++ NewTests/test.py
@@ -22,4 +22,3 @@ result = {
 }
 
 
-print(result)
---
+++ review/exercise02.py
@@ -0,0 +1,24 @@
+# for
+
+for i in range(3):
+    print(i)
+
+## if else
+
+note = 9.5
+
+if note > 9:
+    print("APROVADO COM NOTA MÁXIMA")
+elif note >= 6:
+    print("APROVADO")
+else:
+    print("REPROVADO")
+
+
+# while
+
+count = 0
+while count < 3:
+    print(f"contando: {count}")
+    count += 1
+

f96a7eb1c3b608e40bc85f7f1a9093df0110c981
Luis Guilherme da Cruz <guicruz95@hotmail.com>
2026-06-25 12:27:12 -0300

    I created list comprehension and list with conditions

---
+++ review/exercise07.py
@@ -0,0 +1,18 @@
+## old way
+
+quadrados = []
+
+for i in range(5):
+    quadrados.append(i*3)
+
+
+print(quadrados)
+
+quadrados = [i * 2 for i in range(5)]
+print(quadrados)
+
+
+numbers = [1,3,56,8,8,4,3456,3245,234,3,123454]
+evens = [e for e in numbers if e % 2 == 0]
+
+print(evens)
